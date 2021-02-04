fcd37f1d032097073ee9c1ef327c6867d311037e
leny <info@flatland.be>
2021-02-04 12:09:28 +0100

    Reset constants

--- src/core/constants.js
+++ src/core/constants.js
@@ -6,7 +6,7 @@
  * started at 04/02/2021
  */
 
-// export const SESSION_DURATION = 1500; // 25min
-export const SESSION_DURATION = 3; // 25min
+export const SESSION_DURATION = 1500; // 25min
+export const BREAK_DURATION = 300; // 5min
 
 export const NBSP = "\u00a0";
