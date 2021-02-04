7955b7db3f325dc65b27acb1c83af15cf11d802a
leny <info@flatland.be>
2021-02-04 12:09:45 +0100

    Implement break timer in modal

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -6,11 +6,15 @@
  * started at 04/02/2021
  */
 
+import {useCallback} from "react";
 import {createPortal} from "react-dom";
 import PropTypes from "prop-types";
 
+import {NBSP, BREAK_DURATION} from "../../core/constants";
+import {useTimer} from "../../core/hooks/use-timer";
+
 import Button from "../tools/button";
-import {NBSP} from "../../core/constants";
+import Display from "../display/display";
 
 const styles = {
     position: "fixed",
@@ -30,16 +34,30 @@ const Modal = ({show = false, onClose, onRestart}) => {
         return null;
     }
 
+    const [{running, seconds}, {setRunning}] = useTimer(
+        BREAK_DURATION,
+        true,
+        onRestart,
+    );
+
+    const stopThen = useCallback(
+        next => () => {
+            setRunning(false);
+            next();
+        },
+        [setRunning],
+    );
+
     return createPortal(
         <div style={styles}>
             <div className={"box"}>
                 <h4>{"It's over!"}</h4>
                 <p>{"Please, take a short break!"}</p>
-
+                <Display seconds={seconds} running={running} />
                 <div className={"is-flex"}>
-                    <Button label={"Stop"} onClick={onClose} />
+                    <Button label={"Stop"} onClick={stopThen(onClose)} />
                     {NBSP}
-                    <Button label={"Restart"} onClick={onRestart} />
+                    <Button label={"Restart"} onClick={stopThen(onRestart)} />
                 </div>
             </div>
         </div>,
