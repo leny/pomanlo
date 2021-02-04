6fbbe2567ff4cb5be50a7d0e5c034f20a5b7ba48
leny <info@flatland.be>
2021-02-04 09:32:49 +0100

    Scaffold basic react files

---
+++ src/app.js
@@ -0,0 +1,12 @@
+/* leny/pomanlo
+ *
+ * /src/app.js - Main entry point
+ *
+ * coded by leny@BeCode
+ * started at 04/02/2021
+ */
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<p>{"Hello, World!"}</p>, document.querySelector("#app"));
